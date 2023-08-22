0e6989a8927b4eb2aeb2eeac08da4a0bf21ddd2d
Amrendra Vikram Singh <76041208+AmrendraTheCoder@users.noreply.github.com>
2023-08-22 19:12:23 +0000

    hellohunnybuny

--- 01_Start/test.js
+++ 01_Start/test.js
@@ -1,1 +1,77 @@
-console.log("Amrendra")
+// console.log("Amrendra")
+// console.log("Amrendra")
+// const accountId = 144553
+// let accountEmail = "leostereo110@gmail.com"
+// var accountPassward = "12345"
+// accountCity = "Lucknow"
+
+// // accountId = 2
+
+// accountEmail = "hc@hc.com"
+// accountPassward = "123"
+// accountCity = "Delhi"
+
+// console.table([accountEmail, accountPassward, accountCity])
+
+// // prefer not to issue var due to block scope and functional scope
+
+// // spring => uniqueness // object
+// // null // object
+// // undefined // undefined
+
+// // let {score} = req.body
+// console.log(accountId)
+
+// let score = 33;
+
+// console.log(score)
+
+// let score = "33"
+
+// console.log(typeof(score));
+
+// let wantToBeNumber = Number(score); //dont rely on Number()
+
+// console.log(typeof(wantToBeNumber));
+
+// "33abc" => NaN
+// boolean coversion mein empty ko wo false and value ko wo true dikhata hai 
+
+// operations------------->
+
+// let value = 3;
+
+// let negativeValue = - value;
+
+// console.log(negativeValue);
+
+
+
+// non primitve data type : Array, Object, Function
+// Browser Input 
+
+// const id = Symbol(7054107107) ; Unique Id
+
+// Array, Object, Function
+// object key-value pairs mein exist karte hai 
+
+// const heros = [Ironman, Saktiman, Sung-jin-wo] : Array
+
+
+// :Object: 
+// let myObj = {
+//     name : "Amrendra",
+//     age : 22,
+//     number : 7054107107
+// }
+
+// :Function: typeof :: "Object Function"
+// const myFunction = function(){
+//     console.log("Hello World")
+// }
+
+// +++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitve), Heap (Non-Primitive) :::::: ExcaliDraw.com
+
+let myYoutubeName = "Amrendra"
